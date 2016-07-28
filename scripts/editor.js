d89abc6116e3c0bffd2504ea88d2cd3acf05c542
Dmitri <dmitri.murphy@gmail.com>
2016-07-28 16:40:24 -0700

    Adds Character Equivalencies
    This commit adds character equivalencies. This replaces some expressions
    (such as !=) with their unicode equivalency (≠). Any of these
    replacements can be undone by typing <backspace> right after the
    replacement takes place.

--- scripts/editor.js
+++ scripts/editor.js
@@ -170,6 +170,10 @@ exports.setup = function (files, view, fdbk, hideReveal) {
   session.setUseSoftTabs(true);
   session.setMode("ace/mode/grace");
 
+  //Adds character equivalencies to the editor
+  //see function for more info
+  setupCharacterEquivalencies(editor);
+
   //Event to trigger hiding of view if current file is deleted
   $(document).on("hideEditor", function () {
     view.addClass("hidden");
@@ -357,3 +361,124 @@ exports.setup = function (files, view, fdbk, hideReveal) {
 
   return editor;
 };
+
+
+/*
+ ***  Provides Support for Character Equivalencies ***
+ *The function creates new command keys to support automatic conversion between
+ != and ≠ for example. See the 'replacements' obj. for more info.
+ */
+
+function setupCharacterEquivalencies(editor)
+{
+  var cursorMoved = false;  //Global variable to be set when the cursor moves
+                            //after a character replacement occurs
+
+  //Get ace from brace, from browserify
+  var ace = require('brace');
+  //Extract the Range functionality for use
+  var Range = ace.acequire('ace/range').Range;
+
+  //Possible replacements in the editor
+  var replacements =
+  {
+    "!=":"≠",
+    ">=":"≥",
+    "<=":"≤",
+    "->":"→",
+    "[[":"⟦",
+    "]]":"⟧"
+  };
+
+//Possible replacements for undoing the original replace
+  var undoReplace =
+  {
+    "≠":"!=",
+    "≥":">=",
+    "≤":"<=",
+    "→":"->",
+    "⟦":"[[",
+    "⟧":"]]"
+  };
+
+  //Symbols to add as commands
+  var endSymbols = ['=','>','[',']'];
+
+  //****** Add All Character Equivalancies ********//
+  for(var i =0; i<4; ++i)
+  {
+    var a = endSymbols[i];
+    addCharEq(a);
+  }
+
+  function addCharEq(a)
+  {
+    editor.commands.addCommand({
+      name: 'myCommand'+a,
+      bindKey: {win: a,  mac: a},
+      exec: function(editor) {
+        //Insert '=' to support standard functionality
+        editor.insert(a);
+
+        //Set the cursor moved to false to allow backspace replacement
+        cursorMoved = false;
+
+        //Calculate the cursor position
+        var cursor = editor.getCursorPosition();
+
+        //Check if replacement is possible
+        if(cursor.column >= 2){
+
+          //Get the range and the text
+          var cursorLine = new Range(cursor.row, cursor.column-2, cursor.row, cursor.column);
+          var text = editor.session.getTextRange(cursorLine);
+          if(text in replacements)
+          {
+            //Insert the matching symbol
+            editor.session.replace(cursorLine, replacements[text]);
+
+          }
+        }
+      },
+      readOnly: false // false if this command should not apply in readOnly mode
+    });
+  }
+
+
+//****** BACKSPACE Command Key ********//
+  editor.commands.addCommand({
+    name: 'BACK',
+    bindKey: {win: 'backspace',  mac: 'backspace'},
+    exec: function(editor) {
+      var cursor = editor.getCursorPosition();
+
+      //Check if there is text here, then look for !=
+      if(cursor.column >= 1){
+        var cursorLine = new Range(cursor.row, cursor.column-1, cursor.row, cursor.column);
+        var text = editor.session.getTextRange(cursorLine);
+        if(text in undoReplace && !cursorMoved)
+        {
+          //Replace text - return true to signal overridden functionality
+          editor.session.replace(cursorLine,undoReplace[text]);
+          return true;
+        }
+      }
+
+      //Return false to signal regular backspace functionality
+      return false;
+
+    },
+    readOnly: false // false if this command should not apply in readOnly mode
+  });
+
+
+  //****** Cursor Move Check ********//
+  //Editor check to see if the cursor has moved
+  //used to provide dynamic replacement functionality
+  editor.getSession().selection.on('changeSelection', function(e)
+  {
+    cursorMoved = true;
+  });
+
+}
+
