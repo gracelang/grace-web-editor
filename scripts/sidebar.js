0e1e96b985d60014f519f0ba4248b76c834a8db7
Dmitri <dmitri.murphy@gmail.com>
2016-08-08 16:23:04 -0700

    Replaces old buttons with new icons
    This commit replaces the old "new file" and "new folder" buttons with
    new icons.

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -9,7 +9,7 @@ exports.setup = function (editor, sidebar, resize, hideReveal) {
 
   isClicked = false;
   orig = sidebar.width();
-  min = 110;
+  min = 135; //Minimum sidebar width
 
   function store() {
     localStorage.sidebarWidth = sidebar.width();
